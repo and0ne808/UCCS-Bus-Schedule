54b882170598f639f722b11451103d868d0f9692
and0ne808 <and0ne808@gmail.com>
2016-07-26 11:49:16 -0600

    Updated Message to include more info
    From the UCCS website

--- platforms/browser/js/message.js
+++ platforms/browser/js/message.js
@@ -9,4 +9,10 @@ document.write("\
 <p>Services Monday-Friday</p>\
 <p>All times are departure times.</p>\
 <p>There is only one bus on each route. Please plan accordingly.</p>\
+<p>Inclement weather or traffic conditions can affect scheduled stop times.</p>\
+<p>Shuttle service starts on Jun 13th.</p>\
+<p>Last day of service is Aug 5th.</p>\
+<p>No shuttle service on July 4th. Campus is closed.</p>\
+<p>There is no bus service on Saturdays and Sundays.</p>\
+<p>For Lost and Found items call Parking Services at 719-255-3528</p>\
 ");
